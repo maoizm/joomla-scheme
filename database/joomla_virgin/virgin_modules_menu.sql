5ca0c6a755ab39a5091046dbd255804506378695
mao <green.turban@gmail.com>
2017-02-26 09:10:29 +0200

    database: create empty database

---
+++ database/joomla_virgin/virgin_modules_menu.sql
@@ -0,0 +1,38 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               10.2.3-MariaDB-log - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping data for table joomla_virgin.virgin_modules_menu: ~17 rows (approximately)
+/*!40000 ALTER TABLE `virgin_modules_menu` DISABLE KEYS */;
+INSERT INTO `virgin_modules_menu` (`moduleid`, `menuid`) VALUES
+	(1, 0),
+	(2, 0),
+	(3, 0),
+	(4, 0),
+	(6, 0),
+	(7, 0),
+	(8, 0),
+	(9, 0),
+	(10, 0),
+	(12, 0),
+	(13, 0),
+	(14, 0),
+	(15, 0),
+	(16, 0),
+	(17, 0),
+	(79, 0),
+	(86, 0);
+/*!40000 ALTER TABLE `virgin_modules_menu` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
