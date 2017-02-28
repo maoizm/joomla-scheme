3ab21c876d0fcc059e89342582191b753f4c330f
mao <green.turban@gmail.com>
2017-02-28 19:32:13 +0200

    sources: all files

---
+++ site-work/README.md
@@ -0,0 +1,91 @@
+#Joomla 3.x site development -- starlink.ua
+
+*(does not containt core Joomla files)*
+
+## Installation
+
+1. Clone & cd to the new folder
+2. Have your Node & npm installed and in working condition
+3. `npm install`
+
+## Usage
+
+1. Edit source files in `.src/**`
+2. Compile `.src/**` and copy to Joomla folders: `gulp build`
+3. Compile only `mod_starlink`:  `gulp modstarlink:compile`
+    1. Compile only `mod_starlink`'s css:  `gulp modstarlink:compile:css`
+4. Compile & put in `.zip` package ready for installation in Joomla: `gulp modstarlink:build`
+5. Delete and rebuild all production files from .src:
+   `Gulp cleanBuild`
+   
+### Database operations
+
+#### Backup Joomla db to file 
+
+Database will be copied to file `.YYYYMMDD_HHMM.sql.gz`
+
+- ##### Linux
+   `.src/0_database/db2file.sh`
+ 
+- ##### Windows 
+   `.src\0_database\db2file.cmd`
+
+#### Restore Joomla db from file 
+
+- ##### Linux
+   `.src/0_database/file2db.sh .20161201_1901.sql.gz`
+
+### Other
+
+####svg icons
+
+##### include icon library in the php template:
+ 
+```php
+require_once '../media/mod_starlink/images/icons.svg';
+```
+  
+##### insert particular icon in HTML: 
+
+```html
+<svg class="icon"><use xlink:href="#iconCancel" /></svg>
+```
+  
+##### currently available icons:
+
+- common icons:
+  - `#iconCancel`
+  - `#iconCheck`
+  - `#iconChevronLeft`
+  - `#iconChevronRight`
+  - `#iconExpandLess`
+  - `#iconExpandMore`
+  - `#iconPhone`
+
+- social networks' logos:
+  - `#iconFacebook`
+  - `#iconGooglePlus`
+  - `#iconTwitter`
+
+
+#### style/theme icon in CSS 
+      
+```css
+/* base/default style */
+.icon { 
+  height: 2em; 
+  width: 2em; 
+}
+
+/* add custom themes */
+svg.icon--main {
+  fill: blue;
+}
+svg.icon--alert {
+  fill: red;
+}
+```
+
+```html
+<svg class="icon icon--main"><use xlink:href="#iconCheck" /></svg>
+<svg class="icon icon--alert"><use xlink:href="#iconCheck" /></svg>
---
+++ site-work/administrator/logs/foxcontact.php
@@ -0,0 +1,39 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-02-15 13:53:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: date	time	priority	category	message
+2017-02-15	13:53:28	INFO	install	--- Determining if this system is able to draw captcha images ---
+2017-02-15	13:53:28	INFO	install	gd extension found. Let's see if it works.
+2017-02-15	13:53:28	INFO	install	GD Version... [bundled (2.1.0 compatible)]
+2017-02-15	13:53:28	INFO	install	FreeType Support... [1]
+2017-02-15	13:53:28	INFO	install	FreeType Linkage... [with freetype]
+2017-02-15	13:53:28	INFO	install	GIF Read Support... [1]
+2017-02-15	13:53:28	INFO	install	GIF Create Support... [1]
+2017-02-15	13:53:28	INFO	install	JPEG Support... [1]
+2017-02-15	13:53:28	INFO	install	PNG Support... [1]
+2017-02-15	13:53:28	INFO	install	WBMP Support... [1]
+2017-02-15	13:53:28	INFO	install	XPM Support... [1]
+2017-02-15	13:53:28	INFO	install	XBM Support... [1]
+2017-02-15	13:53:28	INFO	install	WebP Support... [1]
+2017-02-15	13:53:28	INFO	install	JIS-mapped Japanese Font Support... []
+2017-02-15	13:53:28	INFO	install	Testing function [imagecreate]... [1]
+2017-02-15	13:53:28	INFO	install	Testing function [imagecolorallocate]... [1]
+2017-02-15	13:53:28	INFO	install	Testing function [imagefill]... [1]
+2017-02-15	13:53:28	INFO	install	Testing function [imageline]... [1]
+2017-02-15	13:53:28	INFO	install	Testing function [imagettftext]... [1]
+2017-02-15	13:53:28	INFO	install	Testing function [imagejpeg]... [1]
+2017-02-15	13:53:28	INFO	install	Testing function [imagedestroy]... [1]
+2017-02-15	13:53:28	INFO	install	Test for: use_gd ok.
+2017-02-15	13:53:29	INFO	install	--- Method choosen to draw captcha images is [use_gd] ---
+2017-02-15	13:53:29	INFO	install	--- Determining if this system is able to query DNS records ---
+2017-02-15	13:53:29	INFO	install	checkdnsrr function found. Let's see if it works.
+2017-02-15	13:53:29	INFO	install	testing function [checkdnsrr]... [1]
+2017-02-15	13:53:29	INFO	install	--- Method choosen to query DNS records is [dns_check] ---
+2017-02-15	14:15:38	INFO	form	A form has been submitted.
+2017-02-15	14:15:38	INFO	form	Form data validation success.
+2017-02-15	14:15:38	INFO	action	Private message sent to Joomla messenger.
+2017-02-15	14:15:38	INFO	action	Enquiry saved to the database.
+2017-02-15	14:15:40	INFO	action	Administrators notification email sent.
+2017-02-15	14:15:40	INFO	action	User confirmation email sent.
